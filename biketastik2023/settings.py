fc8afff3a41f6d0738f25c44b3ece7fa826f6206
iweinacker <>
2023-09-01 15:01:47 +0000

    Deployment commit

--- biketastik2023/settings.py
+++ biketastik2023/settings.py
@@ -30,8 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['biketastic.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost'
+]
 
 # Application definition
 
